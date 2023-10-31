39ca08a209baf688474b1fbfd0485e6d7cf87aaf
XekoFrontend <haocenter@gmail.com>
2023-10-31 10:33:19 +0700

    10:33 am 31/10/2023

--- 100-days-python/day-9-1-exercise.py
+++ 100-days-python/day-9-1-exercise.py
@@ -5,3 +5,23 @@ student_scores = {
     "Draco": 74,
     "Neville": 64
 }
+
+final_grades = {}
+
+
+def student_grades():
+    for name in student_scores:
+        score = student_scores[name]
+        if score > 90:
+            final_grades[name] = "Outstanding"
+        elif score > 80:
+            final_grades[name] = "Exceeds Expectations"
+        elif score > 70:
+            final_grades[name] = "Acceptable"
+        else:
+            final_grades[name] = "Fail"
+
+    return final_grades
+
+
+print(student_grades())
