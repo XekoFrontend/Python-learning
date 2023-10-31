5b51280495800781c281be21be76f175cf027a72
XekoFrontend <haocenter@gmail.com>
2023-10-31 14:56:22 +0700

    adding some comments

--- 100-days-python/day-9-2-exercise-nesting-dictionary.py
+++ 100-days-python/day-9-2-exercise-nesting-dictionary.py
@@ -16,12 +16,15 @@ travel_log = [
 
 
 def add_new_country(country_visited, time_visited, cities_visited):
+
+    # Creating the new dictionary, that is a item to add to the list
     new_country = {}
 
     new_country["country"] = country_visited
     new_country["visits"] = time_visited
     new_country["cities"] = cities_visited
 
+    # Adding the new item to the list
     travel_log.append(new_country)
 
 
