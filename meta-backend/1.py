7670f245ae96fda450b36281152e5c1066f4a794
XekoFrontend <haocenter@gmail.com>
2023-11-22 19:12:27 +0700

    7:12 pm 22/11/2023

--- meta-backend/1.py
+++ meta-backend/1.py
@@ -1,8 +1,64 @@
-sample_dict = {1: "Coffee", 2: "Tea", 3: "Juice"}
+# Make a cup of coffee
+"""Exercise: Make a cup of coffee
+Introduction
+In this exercise, you will practice the use of an algorithm to make a cup of instant coffee. The purpose is to lay out the steps required in order to get the final product. 
 
-# for key in sample_dict:
-#     print(key)
+Instructions"""
+# Step 1: Start with the inputs - what is needed to make a cup of instant coffee?
+# input("How many gram of coffee: ")
+# input("How many mil of water: ")
+# input("do you want hot or ice coffee: ")
 
-for key in sample_dict:
-    print(key, sample_dict[key])
-    # print(str(key) + ": " + value)
+# Step 2: Think about all the steps required in the physical aspect of making a cup of instant coffee.
+# put coffee
+# boil water
+# adding ice
+
+# Step 3: Consider the edge cases of optional things like milk or sugar, some people may want it.
+# input("do you want adding milk or sugar: ")
+# input("How many gram of milk: ")
+# input("How many gram of sugar: ")
+
+# Step 4: The algorithm when complete should have as its final result a cup of coffee.
+# print("this is your milk, sugar, hot, ice coffee")
+
+# Tips: Planning is key with any algorithm. Make sure you have all the steps neatly laid out.
+
+"""Solution algorithm
+
+Input 
+Ingredients required:
+
+Cup
+
+Hot water
+
+Coffee granules
+
+Optional:
+
+Milk
+
+Cream 
+
+Sugar
+
+Output
+A cup of coffee.
+
+Steps
+1. Add drinking water to an electric kettle. 
+
+2. Put the kettle on to boil water.
+
+3. While waiting, prepare coffee.
+
+4. Add coffee granules to the cup.
+
+5. If water is boiled, pour water into a cup, else continue to wait.
+
+6. If milk or cream is required, add and stir.
+
+7. If sugar is required, add and stir.
+
+8. Return coffee."""
